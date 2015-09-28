81bf1b08807b3b5420939bc33c33d11aeb8842e0
Angelo Falchetti <afalchetti@users.noreply.github.com>
2015-09-28 14:47:10 -0300

    New plot mode: average error.
    Although the two current plot methods (filter and smooth) give a
    good sense of how the algorithm performs, there is room for
    improvement.
    Smoothing has a hard time showing the change in performance as the
    navigator moves in time, since it uses the last estimate so all
    information has already been fused into the result. This means that
    "loop closures" are hard to see because when they occur, they change
    the past, hiding themselves from the final result.
    Filtering is different, since it shows information as it is obtained
    in time, so closures can have a big impact in the current estimate.
    The problem is that this plot is too noisy, as the best estimate can
    change rapidly from frame to frame (low redundancy in information about
    new events).
    Loop closures have their biggest impact on the trajectory as a whole
    so measuring the whole trajectory error as time goes by is interesting.
    The new TimedAverage mode accomplish this by averaging the errors
    of the trajectory in time. The plot is smoother than the alternatives
    and clearly shows trends in the error as a result of drift and the
    moments when the navigator closes a loop, instantaneously decreasing
    the average trajectory error.
    
    Exceptions in the subprocesses are now shown in the console instead
    of obscure general messages.
    
    If files are not found for the map or trajectory estimates, their
    plot is just bypassed instead of crashing the program.

--- additional/plot.py
+++ additional/plot.py
@@ -34,6 +34,7 @@ import numpy as np
 import munkres
 import multiprocessing
 import os, sys, shutil, tempfile, zipfile
+import traceback
 
 rcParams['axes.labelsize']  = 9
 rcParams['xtick.labelsize'] = 9
@@ -45,6 +46,12 @@ rcParams['font.family']     = 'serif'
 rcParams['font.serif']      = ['Computer Modern Roman']
 rcParams['text.usetex']     = True
 rcParams['figure.figsize']  = 4, 2.5
+
+# utility function to define an enumeration
+def enum(*values):
+	return type('Enum', (), dict(zip(values, range(len(values)))))
+
+HistMode = enum("Smooth", "Filter", "TimedAverage")
 
 def readscene(descriptor):
 	descriptor = normalizelinefeeds(descriptor)
@@ -63,6 +70,10 @@ def readtrajectory(descriptor):
 	points     = (tuple(float(n) for n in point.split()) for point in descriptor.split("\n") if point)
 	
 	return [{"time": p[0], "pos": np.array((p[1], p[2], p[3]))} for p in points]
+
+def readtrajectoryhistory(descriptor):
+	history = descriptor.split("\n|\n")
+	return [readtrajectory(snapshot.split("\n", 1)[1]) for snapshot in history]
 
 # histfilter -> for every time, use the best estimate at that time, not at the end
 def readfinaltrajectory(descriptor, histfilter):
@@ -114,7 +125,38 @@ def parsedict(descriptor):
 def normalizelinefeeds(text):
 	return text.replace("\r\n", "\n").replace("\r", "\n")
 
-def poseerror(real, estimate):
+def poseerror(real, estimate, histmode = HistMode.Smooth):
+	if histmode == HistMode.Smooth:
+		return poseerror0(real, estimate[-1])
+		
+	elif histmode == HistMode.Filter:
+		return poseerror0(real, [snapshot[-1] for snapshot in estimate])
+		
+	elif histmode == HistMode.TimedAverage:
+		chopreal = []
+		
+		k = 0
+		for i in xrange(len(estimate)):
+			# for each estimate[i], chopreal[i] will be the real array
+			# in the window where the estimate exists, i.e. the future
+			# tail is removed (so an average error makes sense)
+			
+			# to do so, compare against the time of the last estimate pose [-1]
+			while k < len(real) and real[k]["time"] <= estimate[i][-1]["time"]:
+				# as the estimate grows over time, the chop point does as well
+				k = k + 1
+			
+			chopreal.append(real[0:k])
+		
+		time  = [p["time"] for p in estimate[-1]]
+		error = [np.mean(poseerror0(chopreal[i], estimate[i])[1]) for i in xrange(len(estimate))]
+		
+		return (time, error)
+		
+	else:
+		raise NameError("History mode not recognized")
+
+def poseerror0(real, estimate):
 	treal   = [p["time"] for p in real]
 	posreal = [p["pos"]  for p in real]
 	
@@ -163,6 +205,9 @@ def landmarkdistance(x, y, c):
 	return min(c, np.linalg.norm(x - y))
 
 def addgraphs(a, b):
+	if a is None or b is None:
+		return None
+	
 	(ta, fa) = a
 	(tb, fb) = b
 	
@@ -175,24 +220,26 @@ def addgraphs(a, b):
 
 	return (t, fadd)
 
-def plot(poseerror, maperror, posefile, mapfile):	
-	pplot = mp.figure(1)
-	mp.plot(*poseerror)
-	mp.xlabel("Time [s]")
-	mp.ylabel("Pose error [m]")
-	mp.autoscale(tight=True)
-	pplot.subplots_adjust(bottom=0.15)
-	mp.savefig(posefile)
+def plot(poseerror, maperror, posefile, mapfile):
+	if poseerror is not None:
+		pplot = mp.figure(1)
+		mp.plot(*poseerror)
+		mp.xlabel("Time [s]")
+		mp.ylabel("Pose error [m]")
+		mp.autoscale(tight=True)
+		pplot.subplots_adjust(bottom=0.15)
+		mp.savefig(posefile)
+	
+	if maperror is not None:
+		mplot = mp.figure(2)
+		mp.plot(*maperror)
+		mp.xlabel("Time [s]")
+		mp.ylabel("OSPA Map error")
+		mp.autoscale(tight=True)
+		mplot.subplots_adjust(bottom=0.15)
+		mp.savefig(mapfile)
 
-	mplot = mp.figure(2)
-	mp.plot(*maperror)
-	mp.xlabel("Time [s]")
-	mp.ylabel("OSPA Map error")
-	mp.autoscale(tight=True)
-	mplot.subplots_adjust(bottom=0.15)
-	mp.savefig(mapfile)
-
-def processdir(directory, histfilter = False, verbose = False):
+def processdir(directory, histmode = HistMode.Smooth, verbose = False):
 	if verbose:
 		print "  -- reading scene"
 		
@@ -208,28 +255,41 @@ def processdir(directory, histfilter = False, verbose = False):
 	if verbose:
 		print "  -- reading estimate"
 		
-	with open(os.path.join(directory, "estimate.out")) as efile:
-		estimate   = readfinaltrajectory(efile.read(), histfilter)
+	try:
+		with open(os.path.join(directory, "estimate.out")) as efile:
+			estimate = readtrajectoryhistory(efile.read())
+	except:
+		print "     [!] no pose estimate found, skipping pose error calculation"
+		estimate = None
 	
 	if verbose:
 		print "  -- reading maps"
 		
-	with open(os.path.join(directory, "maps.out")) as mfile:
-		maps = readmaps(mfile.read())
+	try:
+		with open(os.path.join(directory, "maps.out")) as mfile:
+			maps = readmaps(mfile.read())
+	except:
+		print "     [!] no map estimate found, skipping map error calculation"
+		maps = None
 	
-	if verbose:
-		print "  -- calculating pose error"
-		
-	perror = poseerror(trajectory, estimate)
+	perror = None
+	merror = None
 	
-	if verbose:
-		print "  -- calculating map error"
-		
-	merror = maperror(scene["map"], maps)
+	if estimate != None:
+		if verbose:
+			print "  -- calculating pose error"
+			
+		perror = poseerror(trajectory, estimate, histmode)
+	
+	if maps != None:
+		if verbose:
+			print "  -- calculating map error"
+			
+		merror = maperror(scene["map"], maps)
 
 	return (perror, merror)
 
-def processfile(datafile, histfilter = False, verbose = False):
+def processfile(datafile, histmode = HistMode.Smooth, verbose = False):
 	tmp     = tempfile.mkdtemp()
 	datadir = os.path.join(tmp, "data")
 
@@ -239,17 +299,23 @@ def processfile(datafile, histfilter = False, verbose = False):
 		datazip.extractall(datadir)
 
 	print "processing", datafile
-	(perror, merror) = processdir(datadir, histfilter, verbose)
+	(perror, merror) = processdir(datadir, histmode, verbose)
 
 	shutil.rmtree(tmp)
 	
-	np.savetxt(datafile + ".pose.data", perror)
-	np.savetxt(datafile  + ".map.data", merror)
+	if perror is not None:
+		np.savetxt(datafile + ".pose.data", perror)
+	
+	if merror is not None:
+		np.savetxt(datafile  + ".map.data", merror)
 
 	return (perror, merror)
 
 def processfile2(args):
-	return processfile(*args)
+	try:
+		return processfile(*args)
+	except:
+		traceback.print_exc(file=sys.stdout)
 	
 def parsearg(argument):
 	parts = argument.split('=', 1)
@@ -259,7 +325,7 @@ def parsearg(argument):
 		return [parts[0].lstrip('-'), parts[1].strip('\'"')]
 
 def print_usage():
-	print "usage: python plot.py [-history=('filter'|'smooth')] pose.pdf map.pdf data1.zip [data2.zip [data3.zip ...]]"
+	print "usage: python plot.py [-history=('filter'|'smooth'|'timed')] pose.pdf map.pdf data1.zip [data2.zip [data3.zip ...]]"
 
 def main():
 	if len(sys.argv) < 3:
@@ -276,17 +342,19 @@ def main():
 		posefile   = sys.argv[2]
 		mapfile    = sys.argv[3]
 		offset     = 4
-		histfilter = (arg1[1] == "filter")
+		histmode = (HistMode.Filter       if (arg1[1] == "filter")
+		       else HistMode.TimedAverage if (arg1[1] == "timed")
+		       else HistMode.Smooth)
 	else:
 		posefile = sys.argv[1]
 		mapfile  = sys.argv[2]
 		offset   = 3
-		histfilter = False
+		histmode = HistMode.Smooth
 	
-	verbose = False
+	verbose = True
 	
 	pool   = multiprocessing.Pool()
-	errors = pool.map(processfile2, [(f, histfilter, verbose) for f in sys.argv[offset:]])
+	errors = pool.map(processfile2, [(f, histmode, verbose) for f in sys.argv[offset:]])
 	n      = len(errors)
 	pool.close()
 	pool.join()
@@ -298,14 +366,17 @@ def main():
 		totalp = addgraphs(totalp, perror)
 		totalm = addgraphs(totalm, merror)
 	
-	totalp = (np.array(totalp[0]), np.array(totalp[1]) / n)
-	totalm = (np.array(totalm[0]), np.array(totalm[1]) / n)
+	if totalp is not None:
+		totalp = (np.array(totalp[0]), np.array(totalp[1]) / n)
+		np.savetxt(posefile + ".data", totalp)
+	
+	if totalm is not None:
+		totalm = (np.array(totalm[0]), np.array(totalm[1]) / n)
+		np.savetxt(mapfile  + ".data", totalm)
 	
 	print "plotting"
 	plot(totalp, totalm, posefile, mapfile)
 	
-	np.savetxt(posefile + ".data", totalp)
-	np.savetxt(mapfile  + ".data", totalm)
 
 if __name__ == '__main__':
 	main()
